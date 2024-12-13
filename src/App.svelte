66a8446c91cd54b8f688544cbb47d502131a2ce7
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2024-12-13 22:14:56 +0100

    feat: structure layout

--- src/App.svelte
+++ src/App.svelte
@@ -2,19 +2,13 @@
 </script>
 
 <main>
-  <div class="navbar bg-base-100">
-    <div class="navbar-start"></div>
-    <div class="navbar-center">
-      <a class="btn btn-ghost text-xl">Maps</a>
-      <a class="btn btn-ghost text-xl">About</a>
-    </div>
-    <div class="navbar-start"></div>
-  </div>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
 </main>
 
 <style>
-
+  main {
+    justify-content: center;
+    display: flex;
+    align-items: center;
+  }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,31 @@ export default {
     extend: {},
   },
   plugins: [
-    require('daisyui'),
-  ],
+    require('daisyui'),],
+  daisyui: {
+    themes: [
+      // set theme in index.html: data-theme="customtheme"
+      'synthwave',
+      'cupcake',
+      {
+        customtheme: {
+          // Navy #0a192f
+          // light navy #112240
+          // lightest navy #233554
+          // Green #64ffda
+          primary: '#233554',
+          secondary: '#233554',
+          accent: '#ffd200',
+          'accent-focus': '#ffffff',
+          neutral: '#191a3e',
+          'base-100': '#ffffff',
+          info: '#cae2e8',
+          success: '#dff2a1',
+          warning: '#f7e488',
+          error: '#f2b6b5'
+        }
+      }
+    ]
+  }
+
 }
