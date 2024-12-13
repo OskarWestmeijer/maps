3644923619c2c8637f408e7d307989ab8dd74365
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2024-12-13 21:43:40 +0100

    feat: initialize svelte, tailwind and daisyui

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+</script>
+
+<main>
+  <div class="navbar bg-base-100">
+    <div class="navbar-start"></div>
+    <div class="navbar-center">
+      <a class="btn btn-ghost text-xl">Maps</a>
+      <a class="btn btn-ghost text-xl">About</a>
+    </div>
+    <div class="navbar-start"></div>
+  </div>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</main>
+
+<style>
+
+</style>
