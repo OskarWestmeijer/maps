2f9dcec286cbe9e4287a1416e7bfc3efaa3167d1
Oskar Westmeijer <26967048+OskarWestmeijer@users.noreply.github.com>
2025-09-25 18:12:23 +0200

    feat: add gallery view (#254)

---
+++ src/lib/maps.ts
@@ -0,0 +1,67 @@
+export const maps = [
+	{
+		slug: 'nl-elevation',
+		src: '/nl12_light.jpg',
+		title: 'Netherlands elevation',
+		type: 'Topographic map',
+		year: '2024',
+		alt: 'Netherlands height map. Created by Oskar Westmeijer with QGIS.',
+		hdLink: 'https://github.com/OskarWestmeijer/maps/blob/main/static/hd/nl12_light.jpg',
+		description: [
+			"Large parts of the Netherlands are below sea level. For centuries, the Dutch have been reclaiming land from the water. Areas reclaimed from the sea are called 'polders'. The technique to dry the land is relatively simple:"
+		],
+		steps: [
+			'The area is cut off from the sea by building a dike around it.',
+			'The water is then pumped out.'
+		],
+		additional: [
+			'The island of Flevoland, created in the 1950s and 1960s, was reclaimed from a body of water known as the IJsselmeer. It is the largest artificial island in the world.'
+		]
+	},
+	{
+		slug: 'de-forest',
+		src: '/de_forrest7_light.jpg',
+		title: 'Germany forest coverage',
+		type: 'Thematic map',
+		year: '2024',
+		alt: 'Germany forest coverage map. Created by Oskar Westmeijer with QGIS.',
+		hdLink: 'https://github.com/OskarWestmeijer/maps/blob/main/static/hd/de_forrest7_light.jpg',
+		description: [
+			'In 2020 Germany was covered by 32% with forest. Germany is home to several famous forests, each with its unique history and significance:'
+		],
+		steps: [
+			'Thüringer Wald (Thuringian Forest) - This forest is near the town of Eisenach, where Martin Luther translated the New Testament into German during his stay at Wartburg Castle.',
+			'Teutoburger Wald (Teutoburg Forest) - The forest has historical significance as the site of the Battle of the Teutoburg Forest (9 CE), where Germanic tribes ambushed and decisively defeated three Roman legions, halting Romes expansion into Germania.',
+			'Schwarzwald (Black Forest) - Famous for its Kuckucksuhren (cuckoo clocks), handcrafted wooden clocks. It is also the setting of the 1980s TV series Die Schwarzwaldklinik (The Black Forest Clinic).'
+		],
+		additional: []
+	},
+	{
+		slug: 'nl-forest',
+		src: '/nl_forest4_light.jpg',
+		title: 'Netherlands forest coverage',
+		type: 'Thematic map',
+		year: '2024',
+		alt: 'Netherlands forest coverage map. Created by Oskar Westmeijer with QGIS.',
+		hdLink: 'https://github.com/OskarWestmeijer/maps/blob/main/static/hd/nl_forest4_light.jpg',
+		description: [
+			'In 2020, the Netherlands had a forest coverage of just 10%, making it the second least forested country in the European Union after Malta (1%). This figure is significantly below the EU average of 39%. The Netherlands is a densely populated country. Despite that, it is home to two notable forests:'
+		],
+		steps: ['Hoge Veluwe National Park', 'Utrechtse Heuvelrug National Park'],
+		additional: []
+	},
+	{
+		slug: 'nl-west-friesland',
+		src: '/nl_west_friesland3_light.jpg',
+		title: 'West Friesland, Netherlands',
+		type: 'Thematic map',
+		year: '2024',
+		alt: 'Map of West Friesland in the Netherlands. Created by Oskar Westmeijer with QGIS.',
+		hdLink: 'https://github.com/OskarWestmeijer/maps/blob/main/static/hd/nl_west_friesland3_light.jpg',
+		description: [
+			'The Frisian tribes historically occupied regions along the North Sea coast, stretching from Denmark through Germany to the northeastern and northwestern parts of the Netherlands. During the Dutch Golden Age in the 16th century, the towns of Hoorn and Enkhuizen flourished, becoming extremely wealthy through trade. West Friesland is a distinct cultural region within todays province of North Holland.'
+		],
+		steps: [],
+		additional: []
+	}
+];
